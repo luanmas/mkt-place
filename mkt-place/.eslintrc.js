e73732d725a29ae656bc05a7c7d6623abeb2c4eb
luan-mascarenhas <luanmas27@gmail.com>
2023-11-05 16:32:00 -0300

    default template project

---
+++ mkt-place/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+        indent: ['error', 4],
+        'comma-spacing': ['error', { before : false, after: true }],
+    },
+};
