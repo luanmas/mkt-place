1100a5b0d81ab506959d9e7797d7058c491adebc
luanmas <luanmas27@gmail.com>
2023-12-17 23:01:46 -0300

    finished basic crud with adonisJS

---
+++ back/app/Models/Product.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Product extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public product_name: string
+
+  @column()
+  public product_description: string
+  
+  @column()
+  public product_price: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
--- back/src/server.ts
+++
@@ -1,8 +0,0 @@
-import router from "./routes/seuNegocio";
-import express from 'express'
-
-const app = express();
-
-app.use("/api" , express.json() , router);
-
-app.listen(3333, () => console.log('server running on port 3333'));
